d5a821e68c7591f3b4c60cdc9642906b3326553a
Luc Princen <luc@chefduweb.nl>
2015-06-02 14:49:39 +0200

    small fix

--- Assets/js/collections/autoload.js
+++ Assets/js/collections/autoload.js
@@ -2,41 +2,46 @@ define([
 	
 	'jquery',
 	'autoload',
+	'imagesloaded',
 	'isotope',
 
-], function( $, Autoload, Isotope ){
+], function( $, Autoload, imagesLoaded, Isotope ){
 
 	var loader = $('#autoloader').clone();
 	$('#autoloader').remove();
 
-	$('.autoload').AutoLoad({
+	$('#main').imagesLoaded( function() {
 
-		message: $('.autoload').data('msg'),
-		postId: $('.autoload').data('post'),
-		column: $('.autoload').data('id'),
-		section: $('.autoload').data('section_id'),
-		pageNumber: $( '.autoload' ).data('page'),
-		loaderContent: loader[0].outerHTML,
-
-		onComplete: function(){
-
-			var page = parseInt( $( '.autoload' ).data('page') ) + 1;
-			$( '.autoload' ).data('page', page );
-
-			if( $( '.autoload' ).hasClass( 'masonry' ) ){
-
-				//recalculate masonry:
-				var iso = new Isotope( '.masonry', {
-					itemSelector: '.block',
-					layoutMode: 'masonry',
-					masonry: {
-						gutter: 15,
-						columnWidth: '.block'
-				  	}
-				});
+		$('.autoload').AutoLoad({
+	
+			message: $('.autoload').data('msg'),
+			postId: $('.autoload').data('post'),
+			column: $('.autoload').data('id'),
+			section: $('.autoload').data('section_id'),
+			pageNumber: $( '.autoload' ).data('page'),
+			loaderContent: loader[0].outerHTML,
+	
+			onComplete: function(){
+	
+				var page = parseInt( $( '.autoload' ).data('page') ) + 1;
+				$( '.autoload' ).data('page', page );
+	
+				if( $( '.autoload' ).hasClass( 'masonry' ) ){
+	
+					//recalculate masonry:
+					var iso = new Isotope( '.masonry', {
+						itemSelector: '.block',
+						layoutMode: 'masonry',
+						masonry: {
+							gutter: 15,
+							columnWidth: '.block'
+					 	}
+					});
+				}
+	
 			}
-
-		}
+	
+		});
 
 	});
 
