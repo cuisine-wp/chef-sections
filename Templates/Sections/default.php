a2867744dafe8af5048944a6cabca2a002d05609
Luc Princen <luc@chefduweb.nl>
2015-07-27 12:36:57 +0200

    Version bump + template fix

--- Templates/Sections/default.php
+++ Templates/Sections/default.php
@@ -6,13 +6,13 @@
 	echo '<div class="'.$class.'" id="'.$section->name.'">';
 
 
-		if( $section->hide_title === false ){
+		if( $section->hide_title === 'false' ){
 			echo '<h2 class="section-title">';
 				echo $section->title;
 			echo '</h2>';
 		}
 
-		if( $section->getProperty( 'hide_container' ) == false )
+		if( $section->getProperty( 'hide_container' ) == 'false' )
 			echo '<div class="container">';
 
 				echo '<div class="column-wrapper column-row '.$section->view.'">';
@@ -21,7 +21,7 @@
 	
 				echo '</div>';
 
-		if( $section->getProperty( 'hide_container' ) == false )
+		if( $section->getProperty( 'hide_container' ) == 'false' )
 			echo '</div>';
 
 	echo '</div>';
