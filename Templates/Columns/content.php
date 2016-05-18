7f4ea3cce71d2f772090e58c21d619d9d98cec1b
Luc Princen <luc@chefduweb.nl>
2016-05-18 10:04:46 +0200

    Use theField for content

--- Templates/Columns/content.php
+++ Templates/Columns/content.php
@@ -2,5 +2,10 @@
 	<?php if( $column->getField( 'title' ) ):?>
 		<h2 itemprop="name"><?php $column->theField( 'title' );?></h2>
 	<?php endif;?>
-	<?php echo '<span itemprop="text">'.$column->getField( 'content' ).'</span>';?>
+
+	<?php if( $column->getField( 'content' ) ):?>
+	<span itemprop="text">
+		<?php $column->theField( 'content' );?>
+	</span>
+	<?php endif;?>
 </div>
