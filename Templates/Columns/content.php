425022ea686b24013a12d9571333b64292bf0d9b
Pascal van Gaal <pascal@chefduweb.nl>
2016-05-26 13:58:32 +0000

    Fixed getField to theField in the content column.

--- Templates/Columns/content.php
+++ Templates/Columns/content.php
@@ -2,5 +2,9 @@
 	<?php if( $column->getField( 'title' ) ):?>
 		<h2 itemprop="name"><?php $column->theField( 'title' );?></h2>
 	<?php endif;?>
-	<?php echo '<span itemprop="text">'.$column->getField( 'content' ).'</span>';?>
+	<?php 
+		echo '<span itemprop="text">';
+			$column->theField( 'content' );
+		echo '</span>';
+	?>
 </div>
