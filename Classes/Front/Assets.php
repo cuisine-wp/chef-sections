8898df1522d1bed2c12727300459b721ab91ed40
Luc Princen <luc@chefduweb.nl>
2016-07-08 15:48:11 +0200

    Added support for ignoring the sass pipeline

--- Classes/Front/Assets.php
+++ Classes/Front/Assets.php
@@ -39,14 +39,26 @@
 				Script::register( 'fitvids', $url.'fitvids.min', false );
 
 				//sass:
-				$url = 'chef-sections/Assets/sass/front/';
-				
-				Sass::register( 'sections-columns', $url.'_columns', false );
-				Sass::register( 'sections-collection', $url.'_collection', false );
-				Sass::register( 'sections-loader', $url.'_loader', false );
-				Sass::register( 'sections-socials', $url.'_socials', false );
-				Sass::register( 'sections-responsive', $url.'_responsive', false );
-				
+				if( !Sass::ignore() ){
+					
+					$url = 'chef-sections/Assets/sass/front/';
+					
+					Sass::register( 'sections-columns', $url.'_columns', false );
+					Sass::register( 'sections-collection', $url.'_collection', false );
+					Sass::register( 'sections-loader', $url.'_loader', false );
+					Sass::register( 'sections-socials', $url.'_socials', false );
+					Sass::register( 'sections-responsive', $url.'_responsive', false );
+
+				}else{
+
+					//we need to ignore sass and enqueue a regular css file:
+					add_action( 'wp_enqueue_scripts', function(){
+
+						wp_enqueue_style( 'chef_sections', Url::plugin( 'chef-sections', true ).'Assets/css/compiled.css' );
+
+					});
+
+				}
 			});
 		}
 	}
